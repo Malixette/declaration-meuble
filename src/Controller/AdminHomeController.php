141d71de6ed255c9292a85826af72800fc928635
Marine <marine@herrada.fr>
2018-05-09 15:22:26 +0000

    contacts maj

--- src/Controller/AdminHomeController.php
+++ src/Controller/AdminHomeController.php
@@ -20,33 +20,17 @@ class AdminHomeController extends Controller
     public function index(HebergementRepository $repoHeb)
     {
         $user = $this->getUser();
-        dump($user);
         $repo = $this->getDoctrine()->getRepository(User::class);
         $repoHeb = $this->getDoctrine()->getRepository(Hebergement::class);
-        //$repoOt = $this->getDoctrine()->getRepository(OfficeTourisme::class);
-        
+
         $hebergements = $repoHeb->findBy(array("user" => $user->getId()));
-        dump($hebergements);
-        
-
-        // $hebergements = $repoHeb->findAll();
-        // $repoOt = $repoOt->findAll();
-
-
-        //$hebergements = $repoHeb->findByTest();
-        //$hebergements = $repoHeb->findBySQL();
-        //$repoOt = $repoOt->findAll();
 
         $mairie = $user->getMairie();
-        //$ot = $mairie->getOfficeTourisme();
-        dump($user);
-        dump($mairie);
-        
+
         return $this->render('admin_home/dashboard-proprio.html.twig', [
             'hebergements'  => $hebergements,
             'mairie'        => $mairie,
             'user'          => $user,
-            //'officeTourisme'=> $officeTourisme,
         ]);
     }
     
@@ -58,12 +42,18 @@ class AdminHomeController extends Controller
         $user = $this->getUser();
         $repo = $this->getDoctrine()->getRepository(User::class);
         $repoHeb = $this->getDoctrine()->getRepository(Hebergement::class);
-
+        $repoMairie = $this->getDoctrine()->getRepository(Mairie::class);
+        
+        $mairie = $user->getMairie();
+        
+        dump($mairie);
+        
         $hebergements = $repoHeb->findAll();
         
         return $this->render('admin_home/dashboard-mairie.html.twig', [
             'hebergements'  => $hebergements,
             'user'          => $user,
+            'mairie'        => $mairie
         ]);
     }
     
