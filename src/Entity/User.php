84fdbdf1cffa438df843395dbc6911c8b3cedc17
Malix Mayer <mayerma91@gmail.com>
2018-05-12 18:31:10 +0000

    navbar fixed

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -351,9 +351,9 @@ class User implements UserInterface, \Serializable
     public function getRoles()
     {
         if ($this->user_role == 2)
-            return ['ROLE_DECLARANT'];
+            return ['ROLE_DECLARANT', 'ROLE_USER'];
         elseif ($this->user_role == 3)
-            return ['ROLE_MAIRIE'];
+            return ['ROLE_MAIRIE', 'ROLE_USER'];
         elseif ($this->user_role == 4)
             return ['ROLE_OT'];
         elseif ($this->user_role > 4)
