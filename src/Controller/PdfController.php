58d4288528368816f60abc56e0967d2efd6d2bfd
Marine <marine@herrada.fr>
2018-06-07 15:43:49 +0000

    champs cerfa

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -29,15 +29,14 @@ class PdfController extends Controller
     {
         $pdf = new \FPDF('P','mm','A4');
         
-        $user = $this->getUser();
-        
+        $hebergement->setHebStatut('validée');
         $repoHeb = $this->getDoctrine()->getRepository(Hebergement::class);
         $repoMairie = $this->getDoctrine()->getRepository(Mairie::class);
         $repoUser = $this->getDoctrine()->getRepository(User::class);
         
-        dump($user);
-        $hebergement->setHebStatut('validée');
- 
+        $mairie = $hebergement->getMairie();
+        $user = $hebergement->getUser();
+
         // ************************* PAGE 1
 $pdf->AddPage();
 $pdf->SetAutoPageBreak(0);
@@ -62,10 +61,10 @@ $pdf->Cell(175,20,utf8_decode('A - IDENTIFICATION DU DÉCLARANT²'),0,1,'C');
 $pdf->SetFont('Times','',14, 'cp1252');
 $pdf->SetY(95);
 
-$pdf->Cell(0,10,utf8_decode('NOM: {{userDeclarant.usernom }}'),0,0,'L');
+$pdf->Cell(0,10,utf8_decode('NOM: ' . $user->getUserNom() ),0,0,'L');
 $pdf->SetY(105);
 
-$pdf->Cell(0,10,utf8_decode('PRENOM: '),0,0,'L');
+$pdf->Cell(0,10,utf8_decode('PRENOM: ' . $user->getUserPrenom() ),0,0,'L');
 
 $pdf->SetY(115);
 $pdf->Cell(0,10,utf8_decode('LE CAS ÉCHÉANT, LE NOM DE LA PERSONNE MORALE:'),0,1,'L');
@@ -74,23 +73,23 @@ $pdf->Cell(0,10,utf8_decode('LE NUMÉRO D\'IDENTIFICATION (SIRET, SIREN):'),0,1,
 $pdf->Cell(0,10,utf8_decode('texte ici'),0,1,'L');
 
 $pdf->SetY(155);
-$pdf->MultiCell(0,7,utf8_decode('ADRESSE: '),0,'L', 0);
+$pdf->MultiCell(0,7,utf8_decode('ADRESSE: '. $user->getUserAdresse() . $user->getUserComplementAdresse()),0,'L', 0);
 
 $pdf->Cell(0,10,utf8_decode('CODE POSTAL:'),0,0,'L');
 $pdf->SetX(46);
-$pdf->Cell(0,10,utf8_decode('cp ici'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode( $user->getUserPostalCode()),0,1,'L');
 
 $pdf->Cell(0,10,utf8_decode('COMMUNE:'),0,0,'L');
 $pdf->SetX(40);
-$pdf->Cell(0,10,utf8_decode('commune ici'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode( $user->getUserCommune() ),0,1,'L');
 
 $pdf->Cell(0,10,utf8_decode('PAYS:'),0,0,'L');
 $pdf->SetX(30);
-$pdf->Cell(0,10,utf8_decode('pays ici'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode( $user->getUserPays() ),0,1,'L');
 
 $pdf->Cell(0,10,utf8_decode('N°TELEPHONE:'),0,0,'L');
 $pdf->SetX(50);
-$pdf->Cell(0,10,utf8_decode('téléphone ici'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode( $user->getUserTelephone() ),0,1,'L');
 
 $pdf->SetY(225);
 
@@ -114,22 +113,22 @@ $pdf->AddPage();
 $pdf->SetY(10);
 $pdf->SetFont('Times','',14, 'cp1252');
 $pdf->Cell(0,10,utf8_decode('COURRIEL:'),0,1,'L');
-$pdf->Cell(0,10,utf8_decode('email ici'),0,1,'L');
-$pdf->MultiCell(0,7,utf8_decode('ADRESSE DU MEUBLÉ DE TOURISME: '),0,'L', 0);
-$pdf->Cell(0,10,utf8_decode('CODE POSTAL:'),0,0,'L');
+$pdf->Cell(0,10,utf8_decode($user->getUserEmail() ),0,1,'L');
+$pdf->MultiCell(0,7,utf8_decode('ADRESSE DU MEUBLÉ DE TOURISME: ' . $hebergement->getHebAdresse() . $hebergement->getHebAdresseComplement() ),0,'L', 0);
+$pdf->Cell(0,10,utf8_decode('CODE POSTAL: '),0,0,'L');
 $pdf->SetX(46);
-$pdf->Cell(0,10,utf8_decode('cp ici'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode($hebergement->getHebCodePostal()),0,1,'L');
 
 $pdf->Cell(0,10,utf8_decode('COMMUNE:'),0,0,'L');
 $pdf->SetX(40);
-$pdf->Cell(0,10,utf8_decode('commune ici'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode($hebergement->getHebCommune()),0,1,'L');
 
 $pdf->SetFont('Times','B',14, 'cp1252');
 $pdf->Cell(175,20,utf8_decode('B - IDENTIFICATION DU MEUBLÉ DE TOURISME'),0,1,'C');
 
 $pdf->SetFont('Times','',14, 'cp1252');
-$pdf->Cell(0,10,utf8_decode('NOMBRE DE PIÈCES COMPOSANT LE MEUBLÉ:'),0,1,'L');
-$pdf->Cell(0,10,utf8_decode($hebergement->getHebNbrPieces() . ' pièces'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode('NOMBRE DE PIÈCES COMPOSANT LE MEUBLÉ: ' . $hebergement->getHebNbrPieces() . ' pièces'),0,1,'L'); 
+
 $pdf->Cell(0,10,utf8_decode('NOMBRE MAXIMAL DE LITS (soit nombre de personnes susceptibles d\'être accueillies dans'),0,1,'L');
 $pdf->Cell(0,5,utf8_decode('le meublé): ' . $hebergement->getHebCouchagesMax() . ' couchages'),0,1,'L');
 
@@ -141,7 +140,7 @@ $pdf->Cell(0,10,utf8_decode('MAISON INDIVIDUELLE'),0,0,'L');
 $pdf->SetX(105);
 $pdf->Cell(0,10,utf8_decode('APPARTEMENT N° :'),0,0,'L');
 $pdf->SetX(165);
-$pdf->Cell(0,10,utf8_decode('ETAGE N° :'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode('ETAGE N° :' . $hebergement->getHebEtage()),0,1,'L');
 
 $pdf->SetY(145);
 $pdf->Cell(0,10,utf8_decode('LE CAS ÉCHÉANT, date de la décision de classement du meublé de tourisme ou de sa labellisation :'),0,1,'L');
@@ -161,12 +160,12 @@ $pdf->Cell(0,10,
 ,0,1,'L');
 
 $pdf->SetY(220);
-$pdf->Cell(0,10,utf8_decode('FAIT A'),0,0,'L');
+$pdf->Cell(0,10,utf8_decode('FAIT A ' . $user->getUserCommune() ),0,0,'L');
 $pdf->SetX(90);
-$pdf->Cell(0,10,utf8_decode('LE'),0,0,'L');
+$pdf->Cell(0,10,utf8_decode('LE '),0,0,'L');
 
 $pdf->SetY(240);
-$pdf->Cell(0,10,utf8_decode('SIGNATURE'),0,0,'L');
+$pdf->Cell(0,10,utf8_decode('SIGNATURE: ' . $user->getUserNom() . ' ' . $user->getUserPrenom() ),0,0,'L');
 
 $pdf->SetY(265);
 $pdf->SetFont('Times','',13, 'cp1252');
@@ -183,59 +182,58 @@ $pdf->Cell(0,10,utf8_decode('CERFA N° 14004*03'),0,1,'L');
 $pdf->Cell(175,20,utf8_decode('RÉPUBLIQUE FRANÇAISE'),0,1,'C');
 $pdf->Cell(0,10,utf8_decode('MAIRIE de'),0,0,'L');
 $pdf->SetX(45);
-$pdf->Cell(0,10,utf8_decode('ville mairie ici'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode( $mairie->getMairieNomTouristique() ),0,1,'L');
 $pdf->Cell(0,20,utf8_decode('Récépissé de déclaration en mairie de location de meublé de tourisme'),0,1,'L');
 
 $pdf->SetFont('Times','',13, 'cp1252');
 $pdf->Cell(0,5,utf8_decode('Il est donné récépissé de la déclaration en mairie de mise en location d\'un meublé de tourisme pour un'),0,1,'L');
-$pdf->Cell(0,5,utf8_decode('accueil maximal de'),0,0,'L');
+$pdf->Cell(0,5,utf8_decode('accueil maximal de ' . $hebergement->getHebCouchagesMax() . ' personnes situé à : ' . $hebergement->getHebCommune() ),0,0,'L');
 $pdf->SetX(55);
 $pdf->Cell(0,5,utf8_decode(''),0,0,'L');
 $pdf->SetX(65);
-$pdf->Cell(0,5,utf8_decode('personnes situé à :'),0,0,'L');
 
 $pdf->SetY(95);
 $pdf->Cell(0,5,utf8_decode('Adresse:'),0,1,'L');
-$pdf->Cell(0,5,utf8_decode('adresse ici'),0,1,'L');
+$pdf->Cell(0,5,utf8_decode( $hebergement->getHebAdresse() ),0,1,'L');
 $pdf->SetY(110);
 $pdf->Cell(0,10,utf8_decode('Code postal:'),0,0,'L');
 $pdf->SetX(45);
-$pdf->Cell(0,10,utf8_decode('[cp ici]'),0,0,'L');
+$pdf->Cell(0,10,utf8_decode( $hebergement->getHebCodePostal() ),0,0,'L');
 $pdf->SetX(75);
 $pdf->Cell(0,10,utf8_decode('Commune:'),0,0,'L');
 $pdf->SetX(100);
-$pdf->Cell(0,10,utf8_decode('[ville ici]'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode( $hebergement->getHebCommune() ),0,1,'L');
 
 $pdf->SetY(125);
 $pdf->Cell(0,10,utf8_decode('NOM, PRENOM du déclarant:'),0,0,'L');
 $pdf->SetX(75);
-$pdf->Cell(0,10,utf8_decode('[nom prenom ici]'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode( $user->getUserNom() . ' ' . $user->getUserPrenom() ),0,1,'L');
 $pdf->Cell(0,5,utf8_decode('Adresse:'),0,1,'L');
-$pdf->Cell(0,5,utf8_decode('[adresse ici]'),0,1,'L');
+$pdf->Cell(0,5,utf8_decode($user->getUserAdresse() ),0,1,'L');
 $pdf->SetY(150);
 $pdf->Cell(0,10,utf8_decode('Code postal:'),0,0,'L');
 $pdf->SetX(45);
-$pdf->Cell(0,10,utf8_decode('[cp ici]'),0,0,'L');
+$pdf->Cell(0,10,utf8_decode($user->getUserPostalCode() ),0,0,'L');
 $pdf->SetX(75);
 $pdf->Cell(0,10,utf8_decode('Commune:'),0,0,'L');
 $pdf->SetX(100);
-$pdf->Cell(0,10,utf8_decode('[villle ici]'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode( $user->getUserCommune() ),0,1,'L');
 $pdf->Cell(0,10,utf8_decode('Pays'),0,0,'L');
 $pdf->SetX(30);
-$pdf->Cell(0,10,utf8_decode('[pays ici]'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode( $user->getUserPays() ),0,1,'L');
 $pdf->Cell(0,10,utf8_decode('Courriel:'),0,0,'L');
 $pdf->SetX(35);
-$pdf->Cell(0,10,utf8_decode('[mail ici]'),0,1,'L');
+$pdf->Cell(0,10,utf8_decode( $user->getUserEmail() ),0,1,'L');
 
 $pdf->SetY(190);
-$pdf->Cell(0,10,utf8_decode('Fait à'),0,0,'L');
+$pdf->Cell(0,10,utf8_decode('Fait à ' . $mairie->getMairieNomTouristique() ),0,0,'L');
 $pdf->SetX(85);
 $pdf->Cell(0,10,utf8_decode(', le'),0,0,'L');
 
 $pdf->SetY(215);
 $pdf->Cell(0,10,utf8_decode('Cachet de la mairie'),0,1,'L');
 $pdf->SetX(30);
-$pdf->Cell(0,10,utf8_decode('[cachet ici]'),0,0,'L');
+$pdf->Cell(0,10,utf8_decode( $mairie->getMairieTampon() ),0,0,'L');
 
 
         
