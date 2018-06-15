42ef511226f5abf7e24c0ad2ad0e7a4a771c92ec
Marine <marine@herrada.fr>
2018-06-15 10:02:57 +0000

    inscription mairie

---
+++ src/Migrations/Version20180615091840.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180615091840 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mairie ADD CONSTRAINT FK_3946A254A73F0036 FOREIGN KEY (ville_id) REFERENCES villes (id)');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mairie DROP FOREIGN KEY FK_3946A254A73F0036');
+    }
+}
